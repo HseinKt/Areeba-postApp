b6b8d342957da31c9fa6db9403b8844523c75d37
Hussein Kteish <hseinkteish1998@gmail.com>
2026-02-20 11:53:58 +0200

    Added backend

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Hussein!';
+    return 'Hello Hussein kbirr!';
   }
 }
